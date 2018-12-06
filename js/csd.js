778c42cee85cb661a09c967929209ed5f60095a5
rorywalsh <rorywalsh@ear.ie>
2018-12-06 20:31:10 +0000

    linking to game

--- js/csd.js
+++ js/csd.js
@@ -46,42 +46,51 @@ endin
 
 ;random platforms
 instr 6
-k1 randi 1, .04, 2
-k2 randi 1, .06, 2
-k3 randi 1, .03, 2
-k4 randi 1, .01, 2
-k5 randi 1, .06, 2
-k6 randi 1, .08, 2
-k7 randi 1, .04, 2
-k8 randi 1, .01, 2
+k1 oscili 1, .05, -1, .1 
+k2 oscili 1, .05, -1, .2
+k3 oscili 1, .05, -1, .3
+k4 oscili 1, .05, -1, .4 
+k5 oscili 1, .05, -1, .5 
+k6 oscili 1, .05, -1, .6 
+k7 oscili 1, .05, -1, .7 
+k8 oscili 1, .05, -1, .8 
 
-chnset abs(k1), "platform1"
-chnset abs(k2), "platform2"
-chnset abs(k3), "platform3"
-chnset abs(k4), "platform4"
-chnset abs(k5), "platform5"
-chnset abs(k6), "platform6"
-chnset abs(k7), "platform7"
-chnset abs(k8), "platform8"
+chnset abs(k1), "platform0"
+chnset abs(k2), "platform1"
+chnset abs(k3), "platform2"
+chnset abs(k4), "platform3"
+chnset abs(k5), "platform4"
+chnset abs(k6), "platform5"
+chnset abs(k7), "platform6"
+chnset abs(k8), "platform7"
 
 
-aOut1 oscili k1*.1, 100, 1
-aOut2 oscili k2*.1, 200, 1
-aOut3 oscili k3*.1, 300, 1
-aOut4 oscili k4*.1, 400, 1
-aOut5 oscili k5*.1, 500, 1
-aOut6 oscili k6*.1, 600, 1
-aOut7 oscili k7*.1, 700, 1
-aOut8 oscili k8*.1, 800, 1
+aOut1 oscili k1, 100, 1
+aOut2 oscili k2, 101, 2
+aOut3 oscili k3, 102, 3
+aOut4 oscili k4, 103, 4
+aOut5 oscili k5, 104, 5
+aOut6 oscili k6, 105, 6
+aOut7 oscili k7, 106, 7
+aOut8 oscili k8, 107, 8
 
 aMix = aOut1+aOut2+aOut3+aOut4+aOut5+aOut6+aOut7+aOut8
 
-outs aMix/10, aMix/10
+outs aMix*.01, aMix*.01
 endin
 
 </CsInstruments>
 <CsScore>
-f1 0 1024 10 1 .5 .25
+f1 0 1024 10 1
+f2 0 1024 10 1 .5
+f3 0 1024 10 1 .5 .2
+f4 0 1024 10 1 .5 .2 .17
+f5 0 1024 10 1 .5 .2 .17 .12
+f6 0 1024 10 1 .5 .2 .17 .12 .10
+f7 0 1024 10 1 .5 .2 .17 .12 .10 .8
+f8 0 1024 10 1 .5 .2 .17 .12 .10 .8 .7
+
+
 f0 z
 </CsScore>
 </CsoundSynthesizer>
