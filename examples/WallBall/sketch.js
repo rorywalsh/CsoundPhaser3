2f0724d4e2ad018ba4b6fa34801404379811d77d
rorywalsh <rorywalsh@ear.ie>
2019-12-06 14:38:39 +0000

    updating

--- examples/WallBall/sketch.js
+++ examples/WallBall/sketch.js
@@ -295,7 +295,7 @@ function draw() {
     text(debugInfo+" "+touches.length, 100, 100);
 
     if(isMobile == true)
-        text("Mobile4", 100, 200);
+        text("Mobil5", 100, 200);
     else
         text("not Mobile", 100, 200);
 }
