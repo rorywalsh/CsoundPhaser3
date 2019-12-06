7064555b389d8ae376fac614ae716d996a4d0338
rorywalsh <rorywalsh@ear.ie>
2019-12-06 11:16:00 +0000

    updating

--- examples/WallBall/sketch.js
+++ examples/WallBall/sketch.js
@@ -1,7 +1,21 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
-// Code for: https://youtu.be/urR596FsU68
+document.addEventListener('gesturestart', function(e) {
+    e.preventDefault();
+    // special hack to prevent zoom-to-tabs gesture in safari
+    document.body.style.zoom = 0.99;
+});
+
+document.addEventListener('gesturechange', function(e) {
+    e.preventDefault();
+    // special hack to prevent zoom-to-tabs gesture in safari
+    document.body.style.zoom = 0.99;
+});
+
+document.addEventListener('gestureend', function(e) {
+    e.preventDefault();
+    // special hack to prevent zoom-to-tabs gesture in safari
+    document.body.style.zoom = 0.99;
+});
+
 
 // module aliases
 var Engine = Matter.Engine,
